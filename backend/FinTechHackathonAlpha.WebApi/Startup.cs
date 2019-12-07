f361e9030a5b5b20b74c4f8b3151f4a4343a17a2
junwen.luo <luojunwenbashen@163.com>
2019-12-07 08:54:31 +0800

    create webapi project

---
+++ backend/FinTechHackathonAlpha.WebApi/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace FinTechHackathonAlpha.WebApi
+{
+    public class Startup
+    {
+	    private const string ApiName = "FinTechHackathonAlpha.WebApi";
+	    private const string ApiVersion = "v1";
+
+		public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+	        services.AddSwaggerGen(c =>
+	        {
+		        c.SwaggerDoc(ApiVersion, new Info
+		        {
+			        Title = ApiName,
+			        Version = ApiVersion,
+			        Description = "Api for Alpha Link App"
+		        });
+	        });
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+	        app.UseSwagger();
+
+	        app.UseSwaggerUI(c =>
+	        {
+		        c.SwaggerEndpoint($"/swagger/{ApiVersion}/swagger.json", $"{ApiName} {ApiVersion}");
+	        });
+
+			if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
