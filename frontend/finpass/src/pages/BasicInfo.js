49d7bc699b224c62e322ea202320311273c6dcb6
Jackal Fan <goanitz723@gmail.com>
2019-12-07 18:41:25 +0800

    update basic info page

--- frontend/finpass/src/pages/BasicInfo.js
+++ frontend/finpass/src/pages/BasicInfo.js
@@ -4,7 +4,38 @@ import './BasicInfo.css';
 export default class BasicInfo extends React.Component {
   render() {
     return (
-      <div>basicInfo</div>
+      <div className="row">
+        <form className="col s12">
+          <div className="row">
+            <div className="input-field col s6">
+              <input id="first_name" type="text" className="validate" />
+              <label htmlFor="first_name">First Name</label>
+            </div>
+            <div className="input-field col s6">
+              <input id="last_name" type="text" className="validate" />
+              <label htmlFor="last_name">Last Name</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input id="id_number" type="text" className="validate" />
+              <label htmlFor="id_number">ID Number</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input id="address" type="text" className="validate" />
+              <label htmlFor="address">Address</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input id="email" type="email" className="validate" />
+              <label htmlFor="email">Email</label>
+            </div>
+          </div>
+        </form>
+      </div>
     );
   }
 }
--- frontend/finpass/src/pages/Profile.js
+++ frontend/finpass/src/pages/Profile.js
@@ -5,16 +5,13 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import M from "materialize-css";
 import logo from '../FinPass.png';
 import './Profile.css';
 import BasicInfo from './BasicInfo';
 import Attachment from './Attachment';
 
 export default class Profile extends React.Component {
-  componentDidMount() {
-    M.Tabs.init(this.Tabs);
-  }
+
   render() {
     return (
       <Router>
@@ -23,8 +20,8 @@ export default class Profile extends React.Component {
               <img src={logo} alt="finPass" className='brand-logo center logo'/> 
             </div>
             <div className="nav-content blue">
-              <ul className="tabs tabs-transparent" ref={Tabs => {this.Tabs = Tabs;}}>
-                <li className="tab active"><Link to="/profile/basic">Basic Info</Link></li>
+              <ul className="tabs tabs-transparent">
+                <li className="tab"><Link to="/profile/basic">Basic Info</Link></li>
                 <li className="tab"><Link to="/profile/attachment">Attachment</Link></li>
               </ul>
             </div>
