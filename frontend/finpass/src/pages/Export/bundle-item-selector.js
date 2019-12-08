a6ef5484efb40da1c6b947a8a69aa811e3b45b70
Joseph Chan <lonqer@gmail.com>
2019-12-08 10:41:36 +0800

    (frontend): looks better in h6 tags

--- frontend/finpass/src/pages/Export/bundle-item-selector.js
+++ frontend/finpass/src/pages/Export/bundle-item-selector.js
@@ -2,9 +2,9 @@ import React, { Fragment } from 'react';
 
 export const BundleItemSelector = ({ items, onChange }) => (
     <Fragment>
-        <h3>
-            Select the data items you want to export into the bundle
-        </h3>
+        <h6>
+            2. Select the data items you want to export into the bundle
+        </h6>
         <form action="#">
             {items.map(({ id, name, checked }) => (
                 <p key={id}>
